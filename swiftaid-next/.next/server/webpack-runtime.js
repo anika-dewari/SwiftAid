497096581398c0a93086c951922bbb6c6d73b7cf
savetree-1 <bisht.ritika19200@gmail.com>
2025-11-11 17:50:27 +0530

    Replace dashboard and not-found pages with auth login
    Removed the dashboard and _not-found pages and their related chunks and manifests. Added new auth/login page and updated build and path manifests to reflect this change.

--- swiftaid-next/.next/server/webpack-runtime.js
+++ swiftaid-next/.next/server/webpack-runtime.js
@@ -194,7 +194,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("d73504488255ab12")
+/******/ 		__webpack_require__.h = () => ("8927c365a7fdbfa8")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -232,11 +232,6 @@
 /******/ 			var r = fn();
 /******/ 			return r === undefined ? result : r;
 /******/ 		}
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/nonce */
-/******/ 	(() => {
-/******/ 		__webpack_require__.nc = undefined;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/require chunk loading */
--- swiftaid-next/.next/static/chunks/webpack.js
+++ swiftaid-next/.next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("492f20a7aa49ba44")
+/******/ 		__webpack_require__.h = () => ("fdaf1468012b0806")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1405,11 +1405,6 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/nonce */
-/******/ 	(() => {
-/******/ 		__webpack_require__.nc = undefined;
 /******/ 	})();
 /******/ 	
 /************************************************************************/
